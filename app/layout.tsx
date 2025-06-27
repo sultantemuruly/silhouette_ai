15afa2d8510fdf1088a7aa9815e7dafd7ce67230
sultantemuruly <sultantemuruly@gmail.com>
2025-06-27 14:20:25 +0500

    feat: add logo in tab

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/Logo.png" type="image/png"/>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
