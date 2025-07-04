acfcb39746508aed7ae1989fd625517b6f6a66d5
sultantemuruly <sultantemuruly@gmail.com>
2025-07-04 16:25:00 +0500

    fixes

--- stores/useCategoryStore.ts
+++ stores/useCategoryStore.ts
@@ -2,6 +2,6 @@ import { create } from "zustand";
 import { CategoryState } from "@/types";
 
 export const useCategoryStore = create<CategoryState>((set) => ({
-  selectedCategory: "write",
+  selectedCategory: "wise-write",
   setCategory: (category) => set({ selectedCategory: category }),
 }));
